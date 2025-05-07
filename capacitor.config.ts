89ee6a5ee438246b287c3e3b2a828224c4a549d0
laurinschmitter <laurin_schmitter@sluz.ch>
2025-05-07 09:50:59 +0200

    rename app to Schnitzeljagd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'schnitzel-jagd',
+  appName: 'Schnitzeljagd',
   webDir: 'www'
 };
 
