a6f99848802f4d16cf7863a9b90fd4750ca0aaa3
markostojkovic00 <marko_stojkovic@sluz.ch>
2025-05-12 10:31:08 +0200

    Add qr code task with barcode scanner

--- src/app/pages/qr-code-task/qr-code-task.page.ts
+++ src/app/pages/qr-code-task/qr-code-task.page.ts
@@ -11,7 +11,10 @@ import {
 } from '@ionic/angular/standalone';
 import { TaskComponent } from '../../components/task/task.component';
 import { RouterLink } from '@angular/router';
+import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner';
+import { Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import { GameService } from '../../services/game.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-qr-code-task',
@@ -32,9 +35,41 @@ import { GameService } from '../../services/game.service';
   ],
 })
 export class QrCodeTaskPage {
+  taskComplete: boolean = false;
   private gameService = inject(GameService);
+  private toastController = inject(ToastController);
 
   constructor() {}
+
+  async showQrScanner() {
+    const { ScanResult } = await CapacitorBarcodeScanner.scanBarcode({
+      android: {},
+      cameraDirection: undefined,
+      hint: Html5QrcodeSupportedFormats.QR_CODE,
+      scanButton: false,
+      scanInstructions: '',
+      scanOrientation: undefined,
+      scanText: '',
+      web: {},
+    });
+
+    if (ScanResult === 'M335@ICT-BZ') {
+      this.taskComplete = true;
+      await this.showToast('Der gescannte QR-Code ist korrekt!');
+    } else {
+      await this.showToast('Der gescannte QR-Code ist falsch.');
+    }
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 5000,
+      position: 'top',
+      color: 'light',
+    });
+    await toast.present();
+  }
 
   async cancelGame() {
     await this.gameService.cancelGame();
